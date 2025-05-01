41afe9cc50afc6aa7b0b1357e58fd362c19ba13a
이경수 <kyoungsoo0301@gmail.com>
2025-05-01 11:49:28 +0900

    update user entity

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity // 이 클래스가 JPA 엔티티(=DB 테이블과 매핑되는 클래스)임을 나타냄. 
         // 즉 이 클래스가 DB테이블로 만들어질 것임을 나타낸다.
 @Table(name = "users")
 @Getter // 모든 필드에 대해 Getter 메서드를 자동으로 생성
+@Setter
 @NoArgsConstructor // 파라미터가 없는 기본 생성자 자동 생성
 @AllArgsConstructor // 모든 필드를 받는 생성자 자동 생성
 @Builder // 객체를 편하게 생성할 수 있는 빌더 패던 지원
